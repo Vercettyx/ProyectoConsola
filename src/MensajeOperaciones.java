3e3521913e430a793b2f83950f3cb0004c05c3f5
Vercetty <Vercetty982001@gmail.com>
2024-12-03 23:48:01 +0100

    MensajeOperaciones.java

---
+++ src/MensajeOperaciones.java
@@ -0,0 +1,7 @@
+public class MensajeOperaciones {
+    public static void main(String[] args) {
+        System.out.println("Bienvenidos a la rama Mensaje");
+        System.out.println("*******************************");
+        System.out.println("Esta saliendo de la rama Hasta luego");
+    }
+}
---
+++ src/Operaciones.java
@@ -0,0 +1,19 @@
+public class Operaciones {
+    public static void main(String[] args) {
+        int x1=2;
+        int x2=4;
+        int resultado;
+        System.out.println("Suma");
+        resultado=x1+x2;
+        System.out.println(resultado);
+        System.out.println("Resta");
+        resultado=x2-x1;
+        System.out.println(resultado);
+        System.out.println("Multiplicacion");
+        resultado=x1*x2;
+        System.out.println(resultado);
+        System.out.println("division");
+        resultado=x2/x1;
+        System.out.println(resultado);
+    }
+}
